39a3beba842d24157f45cf9f422e385ad3b1a669
davidlnowak <nowak.david@gmail.com>
2016-03-22 01:14:45 -0400

    Code Complete
    Coding and final changes to the bouncing ball.

--- bouncing-ball.js
+++ bouncing-ball.js
@@ -15,11 +15,13 @@
 
 // the arguments to the constructor function should represent the BouncingBall's starting position
 var BouncingBall = function (startingX, startingY) {
-
+  this.position = new p5.Vector (startingX, startingY);
+  this.direction = p5.Vector.random2D();
+  this.direction.setMag(2);
 };
 
 BouncingBall.prototype = {
-
+  radius : 10,
   // initialize() is typically only called once, after the object is created.
   initialize: function () {
 
@@ -27,12 +29,30 @@ BouncingBall.prototype = {
 
   // update() should update the state of the object, moving it and updating any properties
   update: function () {
-
+    this.position.add(this.direction);
+    this.checkForBounce();
   },
 
   // display() should draw the object to the canvas, using p5 code
   display: function () {
-
+    noStroke();
+    fill(255, 50);
+    ellipse(this.position.x, this.position.y, this.radius * 2, this.radius * 2);
   },
+
+  checkForBounce: function () {
+    if (this.position.y > height - this.radius) this.bounceY();
+    if (this.position.y < this.radius) this.bounceY();
+    if (this.position.x > width - this.radius) this.bounceX();
+    if (this.position.x < this.radius) this.bounceX();
+  },
+
+  bounceX: function () {
+    this.direction.x *= -1; // this line is the same as: this.direction.x = this.direction.x * -1;
+  },
+
+  bounceY: function() {
+    this.direction.y *= -1; // this line is the same as: this.direction.y = this.direction.y * -1;
+  }
 
 };
